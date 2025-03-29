303cb45848b9e65602d90145e95defb40c5b92e6
Rui-Hao Bi <biruihao@westlake.edu.cn>
2025-03-29 16:56:04 +0800

    implement higher order kernel pade approximation

--- src/mkct/core.py
+++ src/mkct/core.py
@@ -2,7 +2,8 @@ import numpy as np
 from numpy.typing import NDArray
 from scipy.interpolate import pade
 from scipy.integrate import simpson
-import sympy as sp  
+import sympy as sp
+import mpmath as mp
 
 import math
 import warnings
@@ -16,7 +17,7 @@ def autocorr_from_K1_freq(
     Omega_1: complex,
     s_vals: NDArray[np.complex128],
     K1_s: NDArray[np.complex128],
-    eta: float = 1e-6,
+    eta: float = 1E-5,
 ) -> NDArray[np.complex128]:
     """ The laplace transform of the autocorrelation convolution equation is:
         C_AA(s) = C_{AA}(0) / (s - Omega_1 - K1(s))
@@ -27,21 +28,23 @@ def autocorr_from_K1_freq(
 
 def autocorr_convolution_freq(
     t: NDArray[np.float64],
-    K1t: NDArray[np.complex128], 
+    K1t: NDArray[np.complex128],
     Omega_1: complex,
-) -> Tuple[NDArray[np.float64], NDArray[np.complex128]]:    
+) -> Tuple[NDArray[np.float64], NDArray[np.complex128]]:
     # The frequency grid
-    s_vals = 1.j * 2 * np.pi * np.fft.fftfreq(len(t), d=t[1] - t[0])    
-    
+    s_vals = 1.j * 2 * np.pi * np.fft.fftfreq(len(t), d=t[1] - t[0])
+    s_vals = np.fft.fftshift(s_vals)
+
     # Compute the Laplace transform of the kernel via FFT
     K_s = np.fft.fft(K1t) * (t[1] - t[0])
-    
+    K_s = np.fft.fftshift(K_s)
+
     # Solve for the Laplace transform of the autocorrelation function
     C_s = autocorr_from_K1_freq(Omega_1, s_vals, K_s)
-    
+
     # Inverse FFT to get the autocorrelation function
-    C_t = np.fft.ifft(C_s) / (t[1] - t[0])
-    
+    C_t = np.fft.ifft(np.fft.ifftshift(C_s)) / (t[1] - t[0])
+
     return t, C_t
 
 
@@ -54,25 +57,25 @@ def autocorr_from_K1_time1(
     n_steps = len(t)
     dt = t[1] - t[0]
     dt_half = 0.5 * dt
-    
+
     # allocate the results
     C_t = np.zeros(n_steps, dtype=np.complex128)
     conv_integral = np.zeros(n_steps, dtype=np.complex128)
     C_t[0] = 1.0
-    
+
     # use the simpson rule to compute the convolution integral
     for i in range(1, n_steps):
         conv_integral[i] = simpson(C_t[:i] * np.flip(K1t[:i]), x=t[:i])
-        C_t[i] = C_t[i-1] + dt * (Omega_1 * C_t[i-1] + conv_integral[i-1])  
+        C_t[i] = C_t[i-1] + dt * (Omega_1 * C_t[i-1] + conv_integral[i-1])
     return t, C_t
-           
+
 
 def autocorr_convolution(
     Omega_1: complex,
-    K1_func: Union[CvalFunc, Tuple[NDArray[np.float64], NDArray[np.float64]]],  
+    K1_func: Union[CvalFunc, Tuple[NDArray[np.float64], NDArray[np.float64]]],
     tf: float,
     dt: float,
-    domain: str = 'time', 
+    domain: str = 'time',
 ) -> Tuple[NDArray[np.float64], NDArray[np.float64]]:
     """method to calculate the autocorrelation function via memory kernel convolution:
         C_AA(t) = Omega_1 C_{AA}(t) + int_0^t dt' K1(t-t') C_{AA}(t')
@@ -82,30 +85,30 @@ def autocorr_convolution(
 
     Args:
         Omega_1 (complex): first order moment of the correlation function
-        K1_func (Union[CvalFunc, Tuple[NDArray[np.float64], NDArray[np.float64]]]): First order memory kernel   
+        K1_func (Union[CvalFunc, Tuple[NDArray[np.float64], NDArray[np.float64]]]): First order memory kernel
         tf (float): final time
         dt (float): time step
         domain (str, optional): choose to solve the convolution in 'time' or 'frequency' domain. Defaults to 'time'.
 
     Returns:
-        Tuple[NDArray[np.float64], NDArray[np.float64]]: (time, autocorrelation)    
+        Tuple[NDArray[np.float64], NDArray[np.float64]]: (time, autocorrelation)
     """
     # Step 0: Define the output time grid
     t = np.arange(0, tf+dt, dt)
-    
+
     # First parse the kernel function
     # two cases:
     # 1. K1 is a function
     # 2. K1 is given as samples (t_mem, K1(t_mem))
     if callable(K1_func):
-        t_mem = np.copy(t)  
+        t_mem = np.copy(t)
         K1_mem = K1_func(t)
     else:
         try:
             t_mem, K1_mem = K1_func
         except:
             raise ValueError("K1_func should be a function or a tuple of (t_mem, K1_mem)")
-    
+
     # If the user wishes to solve in the frequency domain
     if domain == 'frequency':
         return autocorr_convolution_freq(t, K1_mem, Omega_1)
@@ -117,7 +120,7 @@ def autocorr_convolution(
                 # The grids are the same
                 # return autocorr_from_K1_time1(t, K1_mem, Omega_1)
                 return autocorr_from_K1_time1(t, K1_mem, Omega_1)
-            
+
         # reach here, means the grids do not match
         # There are better ways to use a shorter memory kernel
         # but for now, we will just interpolate the memory kernel
@@ -126,18 +129,18 @@ def autocorr_convolution(
             """ Interpolating the memory kernel to match the output grid. """
             """ This may lead to inaccuracies. """
         )
-        
-        
+
+
         K1_interp = np.interp(t, t_mem, K1_mem)
         return autocorr_from_K1_time1(t, K1_interp, Omega_1)
     else:
-        raise ValueError("domain should be either 'time' or 'frequency'")   
+        raise ValueError("domain should be either 'time' or 'frequency'")
 
 
-# MKCT related functions 
+# MKCT related functions
 def get_Kn_initial(Omega_n: NDArray[np.complex128]) -> NDArray[np.complex128]:
     """Compute the initial condition terms for the memory kernel."""
-    return Omega_n[1:] - Omega_n[0] * Omega_n[:-1]  
+    return Omega_n[1:] - Omega_n[0] * Omega_n[:-1]
 
 
 def get_K_propagator(
@@ -152,9 +155,9 @@ def get_K_propagator(
             raise ValueError("trunc_order should be less than the number of frequencies")
     else:
         raise ValueError(f"trunc_order should be an integer or None. Got {trunc_order=}")
-    
+
     L_K = np.zeros((trunc_order, trunc_order), dtype=np.complex128)
-    
+
     for irow in range(trunc_order):
         L_K[irow, 0] += (-Omega_n[irow])
         if irow < trunc_order - 1:
@@ -163,15 +166,15 @@ def get_K_propagator(
 
 def get_K1s(
     Omega_n: NDArray[np.complex128],
-    Kn_0: NDArray[np.complex128], 
+    Kn_0: NDArray[np.complex128],
     Kn_plus1: Optional[CvalFunc] = None
 ) -> CvalFunc:
     # use sympy to create the expression
     s = sp.symbols('s')
-    
+
     n_plus1 = len(Omega_n)
     n = len(Kn_0)
-    
+
     if n_plus1 != n + 1:
         # if not matching, we need to truncate Omega_n or Kn_0
         if n_plus1 > n + 1:
@@ -181,38 +184,61 @@ def get_K1s(
             Kn_0 = Kn_0[:n_plus1-1]
             n = n_plus1 - 1
     m = n + 1
-            
+
+
     num = 0
     den = 0
-    
+
+    num_expr = ""
     for i in range(1, m):
         num += Kn_0[i-1] * s**(m-1-i)
-    
+        nn = '{' + str(i) + '}'
+        od = '{' + str(m-1-i) + '}'
+        num_expr += f"K_{nn}(0) * s**{od} + "
+    num_expr = num_expr[:-2]
+
     # The denominator
     den = s**(m-1)
+    den_expr = "s**" + '{' + str(m-1) + '}'
     for i in range(1, m):
         den += Omega_n[i-1] * s**(m-1-i)
-        
+        den_expr += f" + Omega_{i} * s**" + '{' + str(m-1-i) + '}'
+
+    # print(f"num = {num_expr}")
+    # print(f"den = {den_expr}")
+
     # The final K1(s)
     K1_s = num / den
-    
+
     # use lambdify to create the function
-    
+
     if Kn_plus1 is None:
         return sp.lambdify(s, K1_s, 'numpy')
     elif callable(Kn_plus1):
         num_func = sp.lambdify(s, num, 'numpy')
         den_func = sp.lambdify(s, den, 'numpy')
-        return lambda s: (Kn_plus1 + num_func(s)) / den_func(s) 
+        return lambda s: (Kn_plus1(s) + num_func(s)) / den_func(s)
     else:
         raise ValueError("Kn_plus1 should be a function or None")
-   
-    
+
+def tilde_Omega_from_Omega_recursive(
+    Omega_n: NDArray[np.complex128],
+) -> NDArray[np.complex128]:
+    tilde_Omega_n = np.zeros_like(Omega_n)
+    tilde_Omega_n[0] = Omega_n[0]
+    for m in range(1, Omega_n.size):
+        tilde_Omega_n[m] = Omega_n[m+1-1]
+        for k in range(1, m+1):
+            tilde_Omega_n[m] -= tilde_Omega_n[m-k] * Omega_n[k-1]
+    return tilde_Omega_n
+
+
 def pade_approx_Knt_func(
-    Omega_n: NDArray[np.complex128],    
+    Omega_n: NDArray[np.complex128],
     tilde_Omega_n: NDArray[np.complex128],
     n: int,
     m: Union[int, Tuple[int, int]],
+    return_pq: bool = False,
 ) -> CvalFunc:
     """Use Pade approximation to compute the n-th order memory kernel.
         - n: order of the memory kernel
@@ -221,18 +247,18 @@ def pade_approx_Knt_func(
     If m is provided as a tuple, the Pade approximant will be of order (m[0], m[1]).
 
     Args:
-        Omega_n (NDArray[np.complex128]): higher order moments of the correlation function    
-        tilde_Omega_n (NDArray[np.complex128]): higher order of auxiliary moments   
+        Omega_n (NDArray[np.complex128]): higher order moments of the correlation function
+        tilde_Omega_n (NDArray[np.complex128]): higher order of auxiliary moments
         n (int): order of the memory kernel
-        m (Union[int, Tuple[int, int]]): maximum derivatives of Kn(t) to use in the approximation   
+        m (Union[int, Tuple[int, int]]): maximum derivatives of Kn(t) to use in the approximation
 
     Returns:
         CvalFunc: Pade approximant of the n-th order memory kernel
     """
     # First assert that tilde_Omega_0 == Omega_1
     if tilde_Omega_n[0] != Omega_n[0]:
-        raise ValueError("tilde_Omega_n[0] should be equal to Omega_n[1]")  
-    
+        raise ValueError("tilde_Omega_n[0] should be equal to Omega_n[1]")
+
     if isinstance(m, int):
         m1 = m2 = None
     elif isinstance(m, tuple):
@@ -240,20 +266,20 @@ def pade_approx_Knt_func(
         m = m1 + m2 + 1
     else:
         raise ValueError("m should be an integer or a tuple of integers")
-    
-    # assert that the length of Omega_n and tilde_Omega_n 
+
+    # assert that the length of Omega_n and tilde_Omega_n
     # suffice to compute Knt with order m and n
     if len(Omega_n) < m + n + 1:
         raise ValueError("Omega_n should have at least m + n + 1 elements")
-    
-    
+
+
     # Then compute the initial value of Kn(t)
     # Kn_0 = Omega_{n+1} - Omega_1 * Omega_n
     Kn_0 = Omega_n[n+1-1] - Omega_n[0] * Omega_n[n-1]
-    
+
     # allocate the memory for the polynomial
     Kn_poly = np.zeros(m, dtype=np.complex128)
-    
+
     # evaluate the taylor series polynomial
     for i in range(m):
         if i == 0:
@@ -262,16 +288,60 @@ def pade_approx_Knt_func(
             Kn_poly[i] = Omega_n[n+i+1-1]
             for j in range(i+1):
                 Kn_poly[i] -= tilde_Omega_n[i-j] * Omega_n[n+j-1]
-                
-    # Scale the polynomial by the factorial 
+
+    # Scale the polynomial by the factorial
     for i in range(m):
-        Kn_poly[i] /= math.factorial(i)  
-        
+        Kn_poly[i] /= math.factorial(i)
+
     # compute the Pade approximant
     if m2 is None:
         p, q = pade(Kn_poly, m//2+1)
     else:
         p, q = pade(Kn_poly, m2, m1)
-        
-    return lambda t: p(t) / q(t)
-                
+
+    if return_pq:
+        return lambda t: p(t) / q(t), p, q
+    else:
+        return lambda t: p(t) / q(t)
+
+# def vectorize(mp_math_func: Callable) -> Callable:
+#     """Vectorize a mpmath function to work with numpy arrays."""
+#     def vectorized_func(x: NDArray[np.complex128]) -> NDArray[np.complex128]:
+#         if np.isscalar(x):
+#             # If the input is a scalar, just call the mpmath function
+#             return np.complex128(mp_math_func(x))
+
+#         # Convert the input to a list of mpmath.mpf objects
+#         x_mpf = [mp.mpc(val.real, val.imag) for val in x]
+#         # Call the mpmath function
+#         result_mpf = [mp_math_func(x) for x in x_mpf]
+#         # Convert the result back to a numpy array
+#         return np.array(result_mpf, dtype=np.complex128)
+#     return vectorized_func
+
+
+# def rational_fraction_laplace(p, q):
+#     # Define the symbols
+#     t, s = sp.symbols('t s')
+
+#     # Evaluate the polynomail at t
+#     p_t_expr = sum(p[i] * t**(p.order - i) for i in range(p.order+1))
+#     q_t_expr = sum(q[i] * t**(q.order - i) for i in range(q.order+1))
+#     f_t = sp.expand(p_t_expr / q_t_expr)
+#     print(f"Done evaluating the polynomial")
+#     print(f"f(t) = {f_t}")
+
+#     # Compute the Laplace transform
+#     f_s = sp.laplace_transform(f_t, t, s, noconds=True)
+#     print(f"Done laplace transform")
+#     print(f"f(s) = {f_s}")
+
+#     # lambdify the result with mpmath
+#     f_s_func = sp.lambdify(s, f_s, 'mpmath')
+
+#     # wrap the mpmath function as a numpy vectorized function
+#     # argument type is np.complex128
+#     # f_s_func = np.vectorize(lambda s: np.complex128(f_s_func), signature='(n)->(n)')
+#     return vectorize(f_s_func)
+
+
--- src/mkct/mkct_solver.py
+++ src/mkct/mkct_solver.py
@@ -3,12 +3,14 @@ import numpy as np
 from numpy.typing import NDArray
 from scipy.linalg import expm
 from scipy.sparse.linalg import expm_multiply
+import matplotlib.pyplot as plt
 
 from mkct.core import autocorr_convolution
 from mkct.core import get_Kn_initial
 from mkct.core import get_K_propagator
 from mkct.core import get_K1s
 from mkct.core import pade_approx_Knt_func
+from mkct.core import tilde_Omega_from_Omega_recursive
 
 from dataclasses import dataclass
 from typing import List, Tuple, Callable, Union
@@ -22,43 +24,39 @@ class MKCT_solver:
     Kn_0: NDArray[np.complex128]
     L_K: NDArray[np.complex128]
     rescale: float
-    
+    t: NDArray[np.float64] = None
+    K1t: NDArray[np.complex128] = None
+
     @classmethod
     def init(
         cls,
         Omega_n: NDArray[np.complex128],
-        tilde_Omega_n: NDArray[np.complex128]=None,
         rescale: float=1.0,
     ) -> 'MKCT_solver':
         # compute the initial condition
         Kn_0 = get_Kn_initial(Omega_n)
-        
-        # Compute the maximum possible order of the memory kernel   
+
+        # Compute the maximum possible order of the memory kernel
         # propagator matrix
         L_K = get_K_propagator(Omega_n)
-        
-        # tilde_Omega_0 = Omega_1
-        if tilde_Omega_n is None:
-            tilde_Omega_n = np.array([Omega_n[0]])
-        else:
-            tilde_Omega_n = np.concatenate(([Omega_n[0]], tilde_Omega_n))
-        
-        
+
+        tilde_Omega_n = tilde_Omega_from_Omega_recursive(Omega_n)
+
         return cls(Omega_n, tilde_Omega_n, Kn_0, L_K, rescale)
-    
+
     @property
-    def max_kernel_order(self) -> int:  
+    def max_kernel_order(self) -> int:
         return len(self.Kn_0)
-    
+
     @property
     def max_deriv_order(self) -> int:
         return len(self.tilde_Omega_n) - 1
-    
+
     @staticmethod
     def tab_str(
-        string: str, 
+        string: str,
         N: int=69,
-        border: str='!', 
+        border: str='!',
         tab: str='=',
         fill_tab: str=' ',
     ) -> str:
@@ -66,32 +64,32 @@ class MKCT_solver:
             return border + tab * (N-2) + border
         else:
             left_margin = (N - len(string)) // 2
-            right_margin = N - len(string) - left_margin    
+            right_margin = N - len(string) - left_margin
             return border + fill_tab * left_margin + string + fill_tab * right_margin + border + "\n"
-    
+
     def give_size_hint_str(self) -> str:
         m_max = self.max_deriv_order
         n_plus_m_plus_1 = len(self.Omega_n)
         n_max = n_plus_m_plus_1 - m_max - 1
-        
+
         hint_str = self.tab_str(" Size Hint ", fill_tab='=')
         hint_str += self.tab_str(f"Given {n_plus_m_plus_1} Ω_n, and {m_max} tilde_Ω_n,")
         # hint_str += self.tab_str(f"You can compute the Pade approximant up to order {n_max},")
         # hint_str += self.tab_str(f"with a maximum Pade approximant order of {m_max}.")
-        # hint_str += self.tab_str(f"Highest order kernel which can use all the tilde_Ω_n is {m_max}.")   
+        # hint_str += self.tab_str(f"Highest order kernel which can use all the tilde_Ω_n is {m_max}.")
         hint_str += self.tab_str(f"You can evaluate K_1(t) to K_{n_max}(t) with {m_max} derivatives,")
         hint_str += self.tab_str(f"or K_n(t), n < {n_plus_m_plus_1-1}, with less than {m_max} derivatives.")
         hint_str += self.tab_str("=", fill_tab='=')
         return hint_str
-    
+
     def __str__(self) -> str:
         cls_name = self.__class__.__name__
         cls_str = f"{cls_name}({self.Omega_n.size} Ω_n, {self.tilde_Omega_n.size} tilde_Ω_n, rescale={self.rescale})\n"
         size_hint = self.give_size_hint_str()
         return cls_str + "\n" + size_hint
-   
+
     def solve_hardtruncation(
-        self, 
+        self,
         tf: float,
         dt: float,
         kernel_domain: str='time',
@@ -100,7 +98,7 @@ class MKCT_solver:
         t = np.arange(0, tf+dt, dt)
         t_rescaled = t / self.rescale
         dt_rescaled = dt / self.rescale
-        
+
         # First, compute the memory kernel
         if kernel_domain == 'time':
             # compute the memory kernel via expm_multiply
@@ -111,16 +109,16 @@ class MKCT_solver:
             K1_s_func = get_K1s(Omega_n=self.Omega_n, Kn_0=self.Kn_0)
             s = 1.j * 2 * np.pi * np.fft.fftfreq(t.size, d=dt_rescaled)
             K1_s = K1_s_func(s)
-            
+
             # compute the inverse FFT to get the memory kernel in time domain
-            K1_t = np.fft.ifft(K1_s) / dt_rescaled  
+            K1_t = np.fft.ifft(K1_s) / dt_rescaled
         else:
             raise ValueError(
                 """Invalid domain for the memory kernel."""
                 """Choose either 'time' or 'frequency'."""
-                """Got: {kernel_domain}"""  
+                """Got: {kernel_domain}"""
             )
-            
+
         # Next, compute the convolution
         _, C = autocorr_convolution(
             Omega_1=self.Omega_n[0],
@@ -129,31 +127,79 @@ class MKCT_solver:
             dt=dt/self.rescale,
             domain=conv_domain,
         )
+
+        self.K1t = K1_t
         return t, C
-        
+
     def solve_pade(
         self,
         tf: float,
         dt: float,
         kernel_order: int,
         pade_order: Union[int, Tuple[int, int]],
-        conv_domain: str='time', 
+        conv_domain: str='time',
+        post_ifft_smooth: int = 10,
     ) -> Tuple[NDArray[np.float64], NDArray[np.complex128]]:
         m = sum(pade_order) if isinstance(pade_order, tuple) else pade_order
         assert m <= self.max_deriv_order, f"With {self.max_deriv_order} tilde_Ω_n, the maximum Pade order is {self.max_deriv_order}."
-        
+
         if kernel_order == 1:
             K1_func = pade_approx_Knt_func(
-                Omega_n=self.Omega_n,   
-                tilde_Omega_n=self.tilde_Omega_n,   
+                Omega_n=self.Omega_n,
+                tilde_Omega_n=self.tilde_Omega_n,
                 n=1,
                 m=pade_order,
             )
+            t_rescaled = np.arange(0, tf+dt, dt) / self.rescale
+            dt_rescaled = dt / self.rescale
+            s = 1.j * 2 * np.pi * np.fft.fftfreq(t_rescaled.size, d=dt_rescaled)
+            sp = s
+            K1_t = K1_func(t_rescaled)
+            K1_s = np.fft.fft(K1_t) * dt_rescaled
         else:
-            raise NotImplementedError(
-                "Solving for higher order kernels using Pade approximants is not implemented yet."  
+            Km_func, p, q = pade_approx_Knt_func(
+                Omega_n=self.Omega_n,
+                tilde_Omega_n=self.tilde_Omega_n,
+                n=kernel_order,
+                m=pade_order,
+                return_pq=True,
             )
-        
+            t_rescaled = np.arange(0, tf+dt, dt) / self.rescale
+            dt_rescaled = dt / self.rescale
+            Km_t = Km_func(t_rescaled)
+            w = np.fft.fftshift(np.fft.fftfreq(t_rescaled.size, d=dt_rescaled)) * 2 * np.pi
+            s = 1.j * w
+            sp = s
+
+            Km_s = np.fft.fftshift(np.fft.fft(Km_t)) * dt_rescaled
+            Km_s_func = lambda ss: np.interp(ss.imag, s.imag, Km_s)
+
+            Km_s_interp = Km_s_func(s)
+
+            Omega_n_in = self.Omega_n[:kernel_order]
+            Kn_0_in = self.Kn_0[:kernel_order-1]
+            K1_s = get_K1s(Omega_n=Omega_n_in, Kn_0=Kn_0_in, Kn_plus1=Km_s_func)(s)
+            K1_t = np.fft.ifft(np.fft.ifftshift(K1_s)) / dt_rescaled
+            K1_func = lambda t: np.interp(t, t_rescaled, K1_t)
+
+        # fig = plt.figure(dpi=300)
+        # ax = fig.add_subplot(111)
+        # # K1_sp = np.fft.fftshift(K1_s)
+
+        # ax.plot(sp.imag, K1_s.real, label="real")
+        # ax.plot(sp.imag, K1_s.imag, label="imag")
+        # L = 100
+        # ax.set_xlim(-L*self.rescale, L*self.rescale)
+        # ax.set_xlabel("w")
+        # ax.set_ylabel("K1(w)")
+        # ax.legend()
+        # plt.show()
+
+
+            # raise NotImplementedError(
+            #     "Solving for higher order kernels using Pade approximants is not implemented yet."
+            # )
+
         _, C1 = autocorr_convolution(
             Omega_1=self.Omega_n[0],
             K1_func=K1_func,
@@ -161,8 +207,14 @@ class MKCT_solver:
             dt=dt/self.rescale,
             domain=conv_domain,
         )
-        
+
+        # if conv_domain == "frequency":
+        #     C1[:post_ifft_smooth] = np.mean(C1[:post_ifft_smooth])
+        #     C1[-post_ifft_smooth:] = np.mean(C1[-post_ifft_smooth:])
+
         t = np.arange(0, tf+dt, dt)
+        self.t = t
+        self.K1t = K1_func(t/self.rescale)
         return t, C1
-        
+
 # %%
